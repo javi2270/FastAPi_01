70893ec724efb033abf09c9458587aaf721ca314
Javier Villafañe <carlosjavier.villafane@estudiantes.unahur.edu.ar>
2024-12-16 23:04:25 -0300

    como acceder a la docs interactiva

--- main.py
+++ main.py
@@ -25,4 +25,8 @@ def read_id(item_id: int, q: Union[str, None] = None ):
 def calcular(operando_1:float, operando_2:float):
     return {'suma: ':operando_1 + operando_2}
 
+'''
+para acceder a la DOCUMENTACION INTERACTIVA: http://127.0.0.1:4007/docs
+
+'''
 
