4fa533355454164541c92d278f398c553f0a0dc5
Javier Villafañe <carlosjavier.villafane@estudiantes.unahur.edu.ar>
2024-12-16 22:37:41 -0300

    agregando rutas con parametros

--- main.py
+++ main.py
@@ -3,9 +3,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
-    return {"Hola": "Mundo"}
+    return {
+        "Hola": "Mundo",
+        "Hello":"World"
+        }
 
+        # Tipos de parametros de rutas
+
+
+#       de Rutas  "/"
+@app.get("/items/{item_id}")
+def read_id(item_id: int, q: Union[str, None] = None ):
+    return {'item_id :': item_id, "q:":q }  
+
+
+#       de Consulta "?","&"
+@app.get("/calculadora")
+def calcular(operando_1:float, operando_2:float):
+    return {'suma: ':operando_1 + operando_2}
 
 
