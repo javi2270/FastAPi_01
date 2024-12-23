4a1886f3c7bcd258c8b28db0b4db73794a929a82
Javier Villafañe <carlosjavier.villafane@estudiantes.unahur.edu.ar>
2024-12-23 13:00:06 -0300

    creando el modelo Item con pydantic

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
+class Item(BaseModel):
+    name:str
+    price:float
+    is_offert: Union[bool,None]= None
+    
 
 
 @app.get("/")
