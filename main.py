8bb412c11b7aee4a9d3027fe512e3c27a8542d0a
Javier Villafañe <carlosjavier.villafane@estudiantes.unahur.edu.ar>
2024-12-26 08:06:15 -0300

    creada la ruta PUT para actualizar un item

--- main.py
+++ main.py
@@ -28,9 +28,14 @@ def read_id(item_id: int, q: Union[str, None] = None ):
 
 
 #       de Consulta "?","&"
-@app.get("/calculadora")
+@app.get('/calculadora')
 def calcular(operando_1:float, operando_2:float):
     return {'suma: ':operando_1 + operando_2}
+
+
+@app.put('/items/{item_id}')
+def uodate_item(item_id: int, item: Item):
+    return {'item_name': item.name, 'item_id': item_id, 'item_price': item.price}
 
 '''
 para acceder a la DOCUMENTACION INTERACTIVA: http://127.0.0.1:4007/docs
